baba20f06fe69a49ed00d839e1b56d85cca22ae3
kipliside <33856341+kipliside@users.noreply.github.com>
2019-09-06 20:31:43 +0700

    Update call.js

--- call.js
+++ call.js
@@ -22,7 +22,10 @@ if(a_to_vh==false)
 var x=Math.floor((Math.random()*2)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2018/11/game-terbaik-dan-terpopuler-di-dunia_8.html"}
-if(x=="2"){xxx="/2018/11/publisher-game-terbaik-di-dunia.html"}a_to_ve[i].href="https://safelink-side.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="2"){xxx="/2018/11/game-android-terbaik-dan-populer.html"}
+if(x=="3"){xxx="/2018/11/game-psp-terbaik-dan-seru-dimainkan-di.html"}
+if(x=="4"){xxx="/2018/11/game-terbaik-dan-terpopuler-di-dunia.html"}
+if(x=="5"){xxx="/2018/11/publisher-game-terbaik-di-dunia.html"}a_to_ve[i].href="https://safelink-side.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
